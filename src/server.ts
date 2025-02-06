6df3a73e69887b1af2b77cc11fa8b9f3b8a492fe
DrTevzadze <dr.davidtevzadze@gmail.com>
2025-02-06 21:25:39 +0400

    remove unnecessary subscription route from server

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 import authRoutes from "./routes/auth";
 import protectedRoutes from "./routes/protected";
-import subscriptionRoutes from "./routes/subscription";
 import companyRoutes from "./routes/company";
 import filesRoutes from "./routes/files";
 import invoiceRoutes from "./routes/invoice";
@@ -16,7 +15,6 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/protected", protectedRoutes);
-app.use("/subscription", subscriptionRoutes);
 app.use("/company", companyRoutes);
 app.use("/files", filesRoutes);
 app.use("/invoice", invoiceRoutes);
